fde78dbfa5b46b529e4a2d141b3dfa35e4752284
George K <georgekakarlis@yahoo.com>
2022-04-04 01:08:35 +0300

    #2 commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<title>my blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+     <article>
+     <?= $post; ?>
+    </article>
+
+
+    <a href="/">Go back</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<title>my blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+<?php foreach ($posts as $post) :?>
+<article>
+    <?= $post; ?>
+</article>
+
+
+
+</body>
